5d90847cdf83c857b2440df70fa2fd0a00dff77f
Mika Valkama <mika.valkama@fractaldust.com>
2020-05-10 00:31:22 +0300

    Bash: check input

--- linux/koodi.sh
+++ linux/koodi.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
 
+function readPositiveInt() {
+  local val
+  until [[ $val =~ ^[0-9]+$ ]]
+        do    
+          echo "Enter a number"
+          read val
+  done
+  
+  return $val;
+}
+
 echo "Hello world!"
 echo $0 $*
 echo $(date)
-echo "Enter a number"
-read val1
-echo "Enter a number"
-read val2
+readPositiveInt
+((val1 = $?))
+readPositiveInt
+((val2 = $?))
+
 ((sum = $val1 + $val2))
 echo "Sum of" $val1 "and" $val2 "is" $sum
+
